800d7bb3830973af534e740727dea3c88ba67cda
Damilola Shopeyin <tommyslipz@gmail.com>
2025-08-17 16:13:30 +0100

    modified text

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -28,7 +28,7 @@ exports.getATask = async (req, res) => {
 };
 
 exports.testTask = async (req, res) => {
-  res.send("Welcome to Barcelona!!!!");
+  res.send("Welcome to Barcelona!!");
 };
 
 exports.createATask = async (req, res) => {
